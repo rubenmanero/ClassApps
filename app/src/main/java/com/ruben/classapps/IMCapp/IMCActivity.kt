16f76af81bcb45b5d9cfee68a622a9248c6a0ea3
Profesor <ruben@jaja.com>
2025-10-14 17:14:24 +0200

    layout IMCactivity terminado

--- app/src/main/java/com/ruben/classapps/IMCapp/IMCActivity.kt
+++ app/src/main/java/com/ruben/classapps/IMCapp/IMCActivity.kt
@@ -1,13 +1,39 @@
 package com.ruben.classapps.IMCapp
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.RangeSlider
 import com.ruben.classapps.R
+import java.lang.Math.pow
 
 class IMCActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 70
+    private var currentAge: Int = 30
+    private var currentHeight: Int = 120
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var tvWeight: TextView
+    private lateinit var btnSubstractWeight: CardView
+    private lateinit var btnAddWeight: CardView
+    private lateinit var tvAge: TextView
+    private lateinit var btnSubstractAge: CardView
+    private lateinit var btnAddAge: CardView
+    private lateinit var btnCalculate: CardView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +42,87 @@ class IMCActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left+32, systemBars.top+32, systemBars.right+32, systemBars.bottom+32)
             insets
+        }
+        initComponents()
+        initListeners()
+        setWeight()
+        setAge()
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById<CardView>(R.id.viewMale)
+        viewFemale = findViewById<CardView>(R.id.viewFemale)
+        tvHeight = findViewById<TextView>(R.id.tvHeight)
+        rsHeight = findViewById<RangeSlider>(R.id.rsHeight)
+        tvWeight = findViewById<TextView>(R.id.tvWeight)
+        btnSubstractWeight = findViewById<CardView>(R.id.substractWeight)
+        btnAddWeight = findViewById<CardView>(R.id.addWeight)
+        tvAge = findViewById<TextView>(R.id.tvAge)
+        btnSubstractAge = findViewById<CardView>(R.id.substractAge)
+        btnAddAge = findViewById<CardView>(R.id.addAge)
+        btnCalculate = findViewById<CardView>(R.id.btnCalculate)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener { setComponentColorMale() }
+        viewFemale.setOnClickListener { setComponentColorFemale() }
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#")
+            currentHeight = df.format(value).toInt()
+            tvHeight.text = "$currentHeight cm"
+        }
+        btnSubstractWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+        btnAddWeight.setOnClickListener {
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubstractAge.setOnClickListener {
+            currentAge -= 1
+            setAge()
+        }
+        btnAddAge.setOnClickListener {
+            currentAge += 1
+            setAge()
+        }
+        btnCalculate.setOnClickListener {
+            val result = calculateIMC()
+            Log.i("IMC", "El IMC es $result")
+            //navigateToResult(result)
+        }
+    }
+
+    private fun calculateIMC(): Double {
+        val df = DecimalFormat("#.##")
+        val imc = currentWeight/pow(currentHeight.toDouble()/100, 2.0)
+        return df.format(imc).toDouble()
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
+    }
+
+    private fun setComponentColorFemale() {
+        if(!isFemaleSelected) {
+            viewFemale.setCardBackgroundColor(getColor(R.color.background_component_selected))
+            viewMale.setCardBackgroundColor(getColor(R.color.background_component))
+            isFemaleSelected = true
+            isMaleSelected = false
+        }
+    }
+
+    private fun setComponentColorMale() {
+        if(!isMaleSelected) {
+            viewMale.setCardBackgroundColor(getColor(R.color.background_component_selected))
+            viewFemale.setCardBackgroundColor(getColor(R.color.background_component))
+            isMaleSelected = true
+            isFemaleSelected = false
         }
     }
 }
